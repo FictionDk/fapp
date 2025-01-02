aa19b6c5d6a033d3bffc30a444207f7fab27240b
fictio <ficito.d2k@gmail.com>
2025-01-02 16:44:16 +0800

    feat: sigle page contorl

--- lib/pages/car_rank.dart
+++ lib/pages/car_rank.dart
@@ -11,16 +11,34 @@ class CarRankView extends StatefulWidget {
 }
 
 class _CarRankViewState extends State<CarRankView> {
+  final _TITLE = "Car selling rank";
+
   int _currPage = 0;
   final int _pageSize = 10;
   String _errMsg = '';
-  List<dynamic> _rankData = [];
+  final List<dynamic> _rankData = [];
   bool _hasMore = true;
+  bool _isLoading = false;
+
+  String? _selectedYear;
+  int? _selectedMonth;
+  String _selectedDate = '202411';
+
+  final List<String> _years = List.generate(10, (index) => DateTime.now().year - index).map((e) => e.toString()).toList();
+  final List<int> _months = List.generate(12, (index) => index + 1);
+
+  final ScrollController _scrollController = ScrollController();
 
   @override
   void initState() {
     super.initState();
     _getData();
+  }
+
+  @override
+  void dispose(){
+    _scrollController.dispose();
+    super.dispose();
   }
 
   // 触发时间: setState
@@ -34,45 +52,136 @@ class _CarRankViewState extends State<CarRankView> {
       'aid':'1839','app_name': 'auto_web_pc',
       'offset':'$_currPage',
       'count':'$_pageSize',
-      'month':'202411',
+      'month':_selectedDate,
       'rank_data_type':'11',
     });
 
-    var resp = await http.get(url, headers: {
-      "Content-Type": "application/json",
-      "Accept": "application/json",
-    }).timeout(const Duration(seconds: 10));
+    setState(() {
+      _isLoading = true;
+    });
 
-    if(resp.statusCode == 200){
-      final List<dynamic> fetched = jsonDecode(resp.body)['data']['list'];
+    try{
+      var resp = await http.get(url, headers: {
+        "Content-Type": "application/json",
+        "Accept": "application/json",
+      }).timeout(const Duration(seconds: 10));
+
       setState(() {
-        _rankData = fetched;
-        _currPage++;
+        if(resp.statusCode == 200){
+          final List<dynamic> fetched = jsonDecode(resp.body)['data']['list'];
+          _rankData.addAll(fetched);
+          _currPage++;
+          _hasMore = fetched.length >= _pageSize;
+        }else{
+          _errMsg = "请求失败,${resp.body}";
+        }
       });
-      _hasMore = fetched.length >= _pageSize;
-    }else{
-      _errMsg = "请求失败,${resp.body}";
+
+      WidgetsBinding.instance.addPostFrameCallback((_){
+        _scrollController.animateTo(
+            _scrollController.position.maxScrollExtent,
+            duration: const Duration(microseconds: 500),
+            curve: Curves.easeInOut);
+      });
+    }catch(e){
+      setState(() {
+        _errMsg = "请求失败,${e.toString()}";
+      });
+    }finally {
+      setState(() {
+        _isLoading = false;
+      });
+    }
+  }
+
+  void _reset(){
+    setState(() {
+      _rankData.clear();
+    });
+    _getData();
+  }
+
+  void _updateSelectedDate() {
+    if (_selectedYear != null && _selectedMonth != null) {
+      setState(() {
+        _selectedDate = '$_selectedYear${_selectedMonth.toString().padLeft(2, '0')}';
+      });
     }
   }
 
   Widget _buildList(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(title: Text('Car selling rank')),
-      body: _errMsg != '' ? Center(child: Text("$_errMsg"),) : ListView.builder(
-        itemCount: _rankData.length,
-        itemBuilder:(context,idx){
-          final item = _rankData[idx];
-          return ListTile(
-            leading: Image.network(item['image']),
-            title: Text("${item['series_name']},${item['count']}"),
-          );
-        }),
+      appBar: AppBar(
+        title: Text(_TITLE),
+        actions: [
+          PopupMenuButton(
+            onSelected: (String result){
+              setState(() {
+                _selectedYear = result;
+                _updateSelectedDate();
+              });
+            },
+            itemBuilder: (BuildContext context) => _years.map((String year){
+              return PopupMenuItem<String>(value: year,child: Text(year),);
+            }).toList(),
+            child: Padding(
+              padding: const EdgeInsets.symmetric(horizontal: 16.0),
+              child: Row(children: [
+                Text(_selectedYear ?? '2024'), const Icon(Icons.arrow_drop_down),
+              ],),
+            ),
+          ),
+          PopupMenuButton(
+            onSelected: (int result){
+              setState(() {
+                _selectedMonth = result;
+                _updateSelectedDate();
+              });
+            },
+            itemBuilder: (BuildContext context)=> _months.map((int month){
+              return PopupMenuItem<int>(value: month,child: Text(DateTime(0, month).month.toString()),);
+            }).toList(),
+            child: Padding(
+              padding: const EdgeInsets.symmetric(horizontal: 16.0),
+              child: Row(children: [
+                Text(_selectedMonth == null ? '11' : DateTime(0, _selectedMonth!).month.toString()),
+                const Icon(Icons.arrow_drop_down),
+            ],),
+            ),
+          ),
+        ],
+      ),
+      body: _errMsg != '' ? Center(child: Text("$_errMsg"),) : Stack(
+        children: [
+          ListView.builder(
+            controller: _scrollController,
+            itemCount: _rankData.length,
+            itemBuilder:(context,idx){
+              final item = _rankData[idx];
+              return ListTile(
+                leading: Image.network(item['image']),
+                title: Text("${item['series_name']},${item['count']}"),
+              );
+            }),
+          if(_isLoading)
+            Container(
+              color: Colors.black.withOpacity(0.5),
+              child: const Center(
+                child: CircularProgressIndicator(),
+              ),
+            )
+        ],
+      ),
+
       floatingActionButton: _hasMore ?
         FloatingActionButton(
           onPressed: _getData,
           tooltip: 'LoadMore',
           child: const Icon(Icons.add),
         ) : null,
+      persistentFooterButtons: <Widget>[
+        ElevatedButton(onPressed: _reset,child: Text('刷新'),),
+      ],
     );
   }
 }
